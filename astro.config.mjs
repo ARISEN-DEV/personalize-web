cc9a5ad78581a6bcb6bea36a0267db3b022fcfce
ARISEN-DEV <rfalconeb@gmail.com>
2025-06-24 11:03:59 -0300

    ADD: Update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arisen-dev.github.io',
   vite: {
       plugins: [tailwindcss()]
 	},
