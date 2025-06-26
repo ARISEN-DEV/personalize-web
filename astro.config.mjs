ed01ff027d782119916f38b087e77cb0624be551
ARISEN-DEV <rfalconeb@gmail.com>
2025-06-26 18:21:21 -0300

    UPDATE: astro config and deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arisen-dev.github.io',
+  base: 'persoweb',
   vite: {
       plugins: [tailwindcss()]
 	},
